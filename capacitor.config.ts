c69d296a711163d8b43e3df5b03c3787c9d8e2b8
GURU PRASAD SAHU <guruprasad9610@rmail.com>
2023-11-16 02:58:19 +0530

    app name change

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Movie-App',
+  appName: 'Moviesverse',
   webDir: 'www',
   server: {
     androidScheme: 'https'
